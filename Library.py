05aa9b96996975f5389f6533471a8884ae9864b6
AhernEYeldarB <ahernebradley@gmail.com>
2020-02-20 19:06:24 +0000

    Fixed self reference for library init

--- Library.py
+++ Library.py
@@ -1,9 +1,9 @@
 class Library(object):
     def __init__(self, libraryID, timeUntilScan, booksPerDay, books):
-        libraryID = 0
-        timeUntilScan = timeUntilScan
-        booksPerDay = booksPerDay
-        books = books
+        self.libraryID = 0
+        self.timeUntilScan = timeUntilScan
+        self.booksPerDay = booksPerDay
+        self.books = books
 
     def __str__(self):
         return "ID:%s Setup:%s BookRate:%s books:%s"%(self.libraryID, self.timeUntilScan, self.booksPerDay, self.books)
